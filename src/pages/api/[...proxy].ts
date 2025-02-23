71f17c135b714e868245791c445b11504f605e8a
Nguyen Phuoc <anhdv@z-soft.com.vn>
2025-02-24 06:45:31 +0700

    cdoe

--- src/pages/api/[...proxy].ts
+++ src/pages/api/[...proxy].ts
@@ -3,7 +3,10 @@ import type { APIRoute } from 'astro';
 export const GET: APIRoute = async (context) => {
   try {
     // Lấy đường dẫn từ yêu cầu
-    const path = context.params.proxy?.join('/') || '';
+    const path = Array.isArray(context.params.proxy)
+      ? context.params.proxy.join('/') // Nếu là mảng, chuyển thành chuỗi
+      : context.params.proxy || '';    // Nếu là chuỗi, sử dụng trực tiếp
+
     const apiUrl = `https://jsonplaceholder.typicode.com/${path}`;
 
     // Gọi API bên ngoài
